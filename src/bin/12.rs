8232d469966f6203977fa6a06a6c1cc07201711f
Steven Lotito <steven.lotito@gmail.com>
2026-09-07 20:48:14 -0700

    day 12 part 1

---
+++ src/bin/12.rs
@@ -0,0 +1,37 @@
+use regex::Regex;
+use serde_json::Value;
+
+advent_of_code::solution!(12);
+
+pub fn part_one(input: &str) -> Option<i64> {
+    let re = Regex::new(r"-?\d+").unwrap();
+    Some(input.trim().lines().fold(0, |acc, line| {
+        acc + re
+            .find_iter(line)
+            .map(|m| m.as_str().parse::<i64>().unwrap())
+            .fold(0, |acc, n| acc + n)
+    }))
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let parsed: Value = serde_json::from_str(input.trim()).unwrap();
+    dbg!(&parsed);
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
