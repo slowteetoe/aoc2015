dce961f47aa9f2f3897f3a07374e3a092474cb41
Steven Lotito <steven.lotito@gmail.com>
2026-09-15 20:24:32 -0700

    day 16 part 1

---
+++ src/bin/16.rs
@@ -0,0 +1,110 @@
+use std::{cell::LazyCell, collections::BTreeMap};
+
+use itertools::Itertools;
+
+advent_of_code::solution!(16);
+
+const COMPOUNDS: LazyCell<Vec<&str>> = LazyCell::new(|| {
+    vec![
+        "children",
+        "cats",
+        "samoyeds",
+        "pomeranians",
+        "akitas",
+        "vizslas",
+        "goldfish",
+        "trees",
+        "cars",
+        "perfumes",
+    ]
+});
+
+pub fn parse_line(input: &str) -> (u64, BTreeMap<String, u64>) {
+    let (auntie, detected) = input.trim().split_once(":").unwrap();
+    let auntie = auntie.replace("Sue ", "").parse::<u64>().unwrap();
+    let mut map = BTreeMap::<String, u64>::new();
+    detected.split(",").for_each(|m| {
+        let (k, v) = m.split_once(": ").unwrap();
+        map.insert(k.trim().to_owned(), v.parse::<u64>().unwrap());
+    });
+    dbg!(&map);
+    (auntie, map)
+}
+
+pub fn target() -> BTreeMap<String, u64> {
+    BTreeMap::from([
+        ("children".to_owned(), 3),
+        ("cats".to_owned(), 7),
+        ("samoyeds".to_owned(), 2),
+        ("pomeranians".to_owned(), 3),
+        ("akitas".to_owned(), 0),
+        ("vizslas".to_owned(), 0),
+        ("goldfish".to_owned(), 5),
+        ("trees".to_owned(), 3),
+        ("cars".to_owned(), 2),
+        ("perfumes".to_owned(), 1),
+    ])
+}
+
+pub fn target_fingerprint() -> u64 {
+    fingerprint(target())
+}
+
+// wanted to do a fingerprint, but that didn't work well for a partial match - easier to just check each element as we process
+pub fn fingerprint(map: BTreeMap<String, u64>) -> u64 {
+    COMPOUNDS.iter().enumerate().fold(0, |acc, (idx, key)| {
+        if map.contains_key(*key) {
+            let mut v = 1 << 5;
+            // we have 10 dimensions with values up to 10 (1010) to encode, use 5th bit as a presence indictor
+            v |= map.get(*key).unwrap() << 5 * idx;
+            acc | v
+        } else {
+            acc
+        }
+    })
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let target = target();
+
+    let fingerprints = input
+        .trim()
+        .lines()
+        .map(|line| parse_line(line))
+        .collect_vec();
+    dbg!(&fingerprints);
+
+    fingerprints
+        .iter()
+        .filter_map(|(aunt, m)| {
+            if m.iter()
+                .all(|(k, v)| target.contains_key(k) && target.get(k).unwrap() == v)
+            {
+                Some(*aunt)
+            } else {
+                None
+            }
+        })
+        .nth(0)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
